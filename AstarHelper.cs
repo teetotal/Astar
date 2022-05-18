500315d234db9149b164345319bb78032aa48ca4
Jung <daejung@JungDaeChun.local>
2022-05-19 00:19:45 +0900

    add helper class

---
+++ AstarHelper.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+public class AstarHelper {
+    private Astar mAstar;
+    private int mHeight { get; set; }
+    private int mWidth { get; set; }
+    public AstarHelper(int[,] map, float defaultCost) {
+        mAstar = new Astar(map, defaultCost);
+        mHeight = map.GetLength(0);
+        mWidth = map.GetLength(1);
+    }
+    public Queue<int[]>? Search(int startX, int startY, int endX, int endY) {
+        return Search(new int[]{startX, startY}, new int[]{endX, endY});
+    }
+    public Queue<int[]>? Search(int[] start, int[] end) {
+        if(start[0] < 0 || end[0] < 0 || start[1] >= mHeight || end[1] >= mHeight) {
+            return null;
+        }
+        Astar.Pos posStart = ConvertToInnerPos(start);
+        Astar.Pos posEnd = ConvertToInnerPos(end);
+
+        Stack<Astar.Pos> route = mAstar.Search(posStart, posEnd);
+        Queue<int[]> ret = new Queue<int[]>();
+        while(route.Count() > 0) {
+            ret.Enqueue(ConvertFromInnerPos(route.Pop()));
+        }
+        return ret;
+    }
+    private Astar.Pos ConvertToInnerPos(int[] pos) {
+        /*
+        x = 6, y = 5
+        [1,0] -> [5, 1]
+        */ 
+        return new Astar.Pos(mHeight - 1 - pos[1], pos[0]);
+    }
+    private int[] ConvertFromInnerPos(Astar.Pos pos) {
+        int[] ret = new int[2];
+        ret[0] = pos.y;
+        ret[1] = mHeight - 1 - pos.x;
+
+        return ret;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,70 +5,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random rand = new Random();
-        const int a1 = 5;
-        const int a2 = 5;
-
-        int[,] map = new int[a1, a2]
+        int[,] map =
         {
-            {0,     -1,      0,      0,      0},
-            {0,     -1,      0,      -1,      0},
-            {0,     -1,      0,      -1,      0},
-            {0,     -1,      0,      -1,      0},
-            {0,     0,      0,      -1,      0}
+            {0,     -1,      0,       0,      0, 0, 0},
+            {0,     -1,      0,      -1,      0, 0, 0},
+            {0,     -1,      0,      -1,      0, 0, 0},
+            {0,     -1,      0,      -1,      0, 0, 0},
+            {0,      0,      0,      -1,      0, 0, 0}
         };
 
-        /*
-        for(int y = 0; y < map.GetLength(0); y++)
-        {
-            for(int x = 0; x < map.GetLength(1); x++)
-            {
-                map[y,x] = rand.Next(5);
+        AstarHelper astar = new AstarHelper(map, 0.1f);
+        var route = astar.Search(1, 1, 6, 4);
+        if(route != null) {
+            while(route.Count() > 0) {
+                int[] pos = route.Dequeue();
+                Console.WriteLine("{0}, {1}", pos[0], pos[1]);
             }
         }
-        */
-        
-        int[,] mapR = new int[a1, a2];
-        
-        Astar a = new Astar(map);
-
-        Queue<Astar.State> q = a.Search(new Astar.Pos(0, 0), new Astar.Pos(a1-1, a2-1));
-        int i = 1;
-
-        Astar.State pre = q.Peek();
-        while(q.Count > 0)
-        {
-            Astar.State state = q.Peek();
-            Astar.Pos pos = state.id;
-            
-            //for debug
-            if(pre.parent.x != -1 && !pre.id.Compare(state.parent))
-                Console.WriteLine(String.Format("Parent: {0}, {1} ID: {2},{3} !", pre.id.x, pre.id.y, state.parent.x, state.parent.y));
-            
-            Console.WriteLine(String.Format("Parent: {0}, {1} ID: {2},{3}", state.parent.x, state.parent.y, state.id.x, state.id.y));
-            
-            pre = state;
-            
-            mapR[pos.x,pos.y] = i++;
-            q.Dequeue();
-        }
-        Print(mapR);
-        Print(map);
-        System.Console.WriteLine("--------");
-    }
-
-    static void Print(int[,] map)
-    {
-        for(int y = 0; y < map.GetLength(0); y++)
-        {
-            for(int x = 0; x < map.GetLength(1); x++)
-            {
-                System.Console.Write(String.Format("{0}\t", map[y,x]));
-            }
-            Console.WriteLine();
-        }
-
-        Console.WriteLine();
     }
 }
 
